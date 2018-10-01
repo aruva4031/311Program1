dbea7decb977054be10cfdd6a6bca52d857d069e
Eli <elmanzo@csumb.edu>
2018-10-01 14:34:52 -0700

    adding main functionality (Pinger, and RTT)

--- PA1_Manzo_Ruvalcaba.py
+++ PA1_Manzo_Ruvalcaba.py
@@ -1,19 +1,28 @@
 from socket import *
 import time
+
 serverName = 'SpiritBreaker'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = "ping"
-round_trip_time = 0
-send_start = 0
-data_received = 0
-clientSocket.settimeout(1.00)
+clientSocket.settimeout(1); # sets the timeout to 1 second
+#round_trip_time = 0
+sequence_number = 1
 
-for x in 10:
-    send_start= time.time()
+while True:
+    start= time.time()
+    message = "PING: " + str(sequence_number) + " " + str(send_start)
     clientSocket.sendto(message.encode(),(serverName, serverPort))
 
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    try:
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        elapsed = time.time() - start
+        round_trip_time += time.time() - elapsed #elapsed time
         print (modifiedMessage.decode())
-        
+        print "Round Trip Time is:" + str(elapsed) + " seconds"
+    except socket.timeout: #if no reply within 1 second
+        print (modifiedMessage.decode())
+        print ('Request timed out')
+
+    if sequence_number > 10
+        break
 clientSocket.close()
