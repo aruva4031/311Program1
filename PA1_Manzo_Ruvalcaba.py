6bcdef55a32d312c9d7059d8eb947a154918123e
Eli <elmanzo@csumb.edu>
2018-10-01 15:29:03 -0700

    fixing some bugs: still need to fix try-except bug

--- PA1_Manzo_Ruvalcaba.py
+++ PA1_Manzo_Ruvalcaba.py
@@ -1,28 +1,27 @@
 from socket import *
 import time
 
-serverName = 'SpiritBreaker'
+serverName = 'DESKTOP-JBHS5BJ'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1); # sets the timeout to 1 second
-#round_trip_time = 0
+round_trip_time = 0
 sequence_number = 1
 
 while True:
     start= time.time()
-    message = "PING: " + str(sequence_number) + " " + str(send_start)
+    message = "PING: " + str(sequence_number) + " " + str(round(start, 3))
     clientSocket.sendto(message.encode(),(serverName, serverPort))
-
     try:
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
         elapsed = time.time() - start
         round_trip_time += time.time() - elapsed #elapsed time
         print (modifiedMessage.decode())
-        print "Round Trip Time is:" + str(elapsed) + " seconds"
+        print ("Round Trip Time is:" + str(round(elapsed,3)) + " seconds")
     except socket.timeout: #if no reply within 1 second
-        print (modifiedMessage.decode())
-        print ('Request timed out')
-
-    if sequence_number > 10
+        print (message)
+        print ("Request timed out")
+    sequence_number += 1
+    if sequence_number > 10:
         break
 clientSocket.close()
