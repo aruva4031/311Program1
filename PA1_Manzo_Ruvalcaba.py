3b88e4b0a753bbafd266fd2ab7047eb76a365713
Alex <aruvalcaba@csumb.edu>
2018-10-03 15:07:05 -0700

    fixed everything else, just need to add comments

--- PA1_Manzo_Ruvalcaba.py
+++ PA1_Manzo_Ruvalcaba.py
@@ -9,9 +9,10 @@ round_trip_times = []
 sequence_number = 1
 packets_lost = 0
 smallRTT = 100
-largeRTT = 0
+largeRTT = -1
 avergRTT = 0
 ################################################################################
+print ("-------------------------------------------------")
 while True:
     start= time.time()
     message = "PING: " + str(sequence_number) + " " + str(round(start, 3))
@@ -19,12 +20,14 @@ while True:
     try:
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
         elapsed = time.time() - start
-        round_trip_times.append(time.time() - elapsed) #elapsed time
+        round_trip_times.append(elapsed) #elapsed time
         print (modifiedMessage.decode())
         print ("Round Trip Time is:" + str(round(elapsed,3)) + " seconds")
+        print ("-------------------------------------------------")
     except timeout: #if no reply within 1 second
         print (message)
         print ("Request timed out")
+        print ("-------------------------------------------------")
         packets_lost = packets_lost + 1
     sequence_number += 1
     if sequence_number > 10:
@@ -38,12 +41,13 @@ for x in round_trip_times:
         smallRTT = x
     avergRTT = avergRTT + x
 ################################################################################
+print ("")
 avergRTT = avergRTT / len(round_trip_times)
-print ("Maximum RTT: " + largeRTT + " seconds")
-print ("Minimum RTT: " + smallRTT + " seconds")
-print ("Average RTT: " + avergRTT + " seconds")
+print ("Maximum RTT: " + str(round(largeRTT, 4)) + " seconds")
+print ("Minimum RTT: " + str(round(smallRTT, 4)) + " seconds")
+print ("Average RTT: " + str(round(avergRTT, 4)) + " seconds")
 if packets_lost == 0:
     print ("Packet loss percentage: 0%")
 else:
-    print ("Packet loss percentage: " + (packets_lost/10) * 100 + "%")
+    print ("Packet loss percentage: " + str((packets_lost/10) * 100) + "%")
 clientSocket.close()
